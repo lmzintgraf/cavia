b4ca81f061c3f7eed68aa49c1745fe67bb8b8985
Tim La Roche <timlaroche@users.noreply.github.com>
2021-03-19 14:23:29 +0000

    Fixed layers bug

--- rl/layers/nmlinear.py
+++ rl/layers/nmlinear.py
@@ -5,11 +5,13 @@ import torch.nn.functional as F
 class NMLinear(nn.Module):
 	def __init__(self, input_channels, output_channels):
 		super(NMLinear, self).__init__()
-		self.weight = nn.Parameter(torch.Tensor(input_channels, output_channels))
+		self.weight = nn.Parameter(torch.Tensor(input_channels, output_channels), requires_grad=True)
 
 		self.ws = nn.Linear(input_channels, 8) # Hardcode 8 for now, TODO: Make this a parameter in constructor this
 		self.wm = nn.Linear(8, 8)
 		self.wn = nn.Linear(8, output_channels)
+
+		self.bias = nn.Parameter(torch.Tensor(output_channels), requires_grad=True)
 
 
 	def forward(self, input):
